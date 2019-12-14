c0a80843ce123d61a2173cf1e440fc3a1617d8c9
unknown <cristian.suarez0322@gmail.com>
2019-12-14 10:41:45 -0500

    commitpri

---
+++ src/proyectoAlgoritmo/Empleado.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoAlgoritmo;
+
+/**
+ *
+ * @author csuarez25
+ */
+public class Empleado extends Persona {
+
+    private String cargo;
+    private String fechaDeIngreso;
+    private String fechaDeSalida;
+    private String tipoContrato;
+    private String ARL;
+    private String EPS;
+    private String cesantias;
+    private String pension;
+
+    public Empleado(String nombre, String apellido, int id, String nacimiento, String direccion, String email, String genero, int NHijos, String estadoCivil, String profesion, String nivelEducativo, String cargo, String fechaDeIngreso, String fechaDeSalida, String tipoContrato, String EPS, String ARL, String cesantias, String pension) {
+        super(nombre, apellido, id, nacimiento, direccion, email, genero, NHijos, estadoCivil, profesion, nivelEducativo);
+        this.cargo = cargo;
+        this.fechaDeIngreso = fechaDeIngreso;
+        this.fechaDeSalida = fechaDeSalida;
+        this.tipoContrato = tipoContrato;
+        this.ARL = ARL;
+        this.EPS = EPS;
+        this.cesantias = cesantias;
+        this.pension = pension;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getFechaDeIngreso() {
+        return fechaDeIngreso;
+    }
+
+    public void setFechaDeIngreso(String fechaDeIngreso) {
+        this.fechaDeIngreso = fechaDeIngreso;
+    }
+
+    public String getFechaDeSalida() {
+        return fechaDeSalida;
+    }
+
+    public void setFechaDeSalida(String fechaDeSalida) {
+        this.fechaDeSalida = fechaDeSalida;
+    }
+
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+
+    public void setTipoContrato(String tipoContrato) {
+        this.tipoContrato = tipoContrato;
+    }
+
+    public String getARL() {
+        return ARL;
+    }
+
+    public void setARL(String ARL) {
+        this.ARL = ARL;
+    }
+
+    public String getEPS() {
+        return EPS;
+    }
+
+    public void setEPS(String EPS) {
+        this.EPS = EPS;
+    }
+
+    public String getCesantias() {
+        return cesantias;
+    }
+
+    public void setCesantias(String cesantias) {
+        this.cesantias = cesantias;
+    }
+
+    public String getPension() {
+        return pension;
+    }
+
+    public void setPension(String pension) {
+        this.pension = pension;
+    }
+
+}
